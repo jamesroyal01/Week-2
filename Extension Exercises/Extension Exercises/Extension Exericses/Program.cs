c40e63e94ae28a8e97579f3c54395b0fd9817fab
James Anderson <30005871@student.toiohomai.ac.nz>
2018-08-02 14:07:12 +1200

    Week 2 > or < 1000

---
+++ Extension Exercises/Extension Exercises/Extension Exericses/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number1, number2 = 0;                                                                        // these are varibales                                                                            
+            int final = 0;
+            String choice;
+
+            Console.WriteLine("Please Enter The First Number");
+            number1 = Convert.ToInt32(Console.ReadLine());                                                   //this makes the number convert to output
+            Console.WriteLine("What Do You Want To Do With These Numbers?: (+, *, / or -)");                 // asking the user for which symbol they want to use
+            choice = Console.ReadLine();
+            Console.WriteLine("Please Enter The Second Number");                                             //user input (asking you to put in the number you want)
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            if (choice == "+")                                                                               //this is where it gets worked out, if you typed in a + it will calculate here
+            {
+                final = number1 + number2;
+            }
+
+            if (choice == "-")
+            {
+                final = number1 - number2;
+            }
+
+            if (choice == "*")
+            {
+                final = number1 * number2;
+            }
+
+            if (choice == "/")
+            {
+                final = number1 / number2;
+            }
+
+            Console.WriteLine("The" + choice + "Was Applied");                                               //the computer telling you the problem was solved
+            Console.WriteLine("The End Result is :{0}\n You Are Welcome \n", final);                         // youre welcome for solving the problem for you
+            Console.WriteLine("Press Any Key To Continue.....");                                             // this keeps the application open until you press any key
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Extension Exercises/Pin Code Exercise/Pin Code Exercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin_Code_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 50 / 10;                                                                       //Change these variables to what you want to see if what you enter is more or less thean 1000
+            int b = 5 * 10;
+            int c = 1000;
+
+            if (a * b + c == 1000)                                                                 //if the asnwer is greater than 1000 this is correct. this is that varibles being multiplied, added, subtracted or divided
+            {
+                Console.WriteLine("The Answer Is Greater Than 1000");
+            }
+            else                                                                                   //else the answer is less then 1000
+            {
+                Console.WriteLine("The Answer Is Less Than 1000");                                 //the computer is telling you that the answer is less than 1000
+            }
+
+            Console.WriteLine("Press Any Key To Continue....");
+
+            Console.ReadKey();                                                                     // press any key to continue/close the application
+            .
+        }
+    }
+}
